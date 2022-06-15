4f4aa70503405742205c6555c56c07c1df06ec71
Ravi <ravikumr0403@gmail.com>
2022-06-15 11:06:21 +0530

    fix: relative time

--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -1,11 +1,15 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import relativeTime from "dayjs/plugin/relativeTime";
 
+dayjs.extend(utc);
 dayjs.extend(relativeTime);
+
 export const getFormattedDate = (date: string) => {
-  console.log(date);
-  if (dayjs(new Date()).diff(dayjs(date), "days") >= 1) {
+  const now = dayjs();
+  const parsedDate = dayjs.utc(date);
+  if (now.diff(parsedDate, "days") >= 1) {
     return dayjs(date).format("MMM DD, YYYY");
   }
-  return dayjs(date).fromNow();
+  return parsedDate.fromNow();
 };
